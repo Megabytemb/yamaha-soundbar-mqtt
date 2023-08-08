7710f81018b4015d5dff951eba624d85e0b764be
Michael Benz <michael@benz.io>
2023-08-08 19:17:10 +1000

    small updates

---
+++ src/__main__.py
@@ -0,0 +1,11 @@
+from src.device import Device
+import anyio
+import logging
+
+async def run():
+    device = Device()
+    await device.run()
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG)
+    anyio.run(run)
